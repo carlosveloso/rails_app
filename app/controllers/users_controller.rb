b841176577cd3067df1be76a2a8860dcdf020349
carlos <carlos.veloso@groupon.cl>
2014-04-23 06:37:46 -0300

    fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,11 +34,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       sign_in @user
       flash[:success] = "Profile updated"
